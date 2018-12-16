5e3b7285e49d7d703f5597ed2be51450851ea732
ejrbuss <ejrbuss@gmail.com>
2018-12-15 19:56:30 -0800

    prep for pip

--- docs/build.js
+++ docs/build.js
@@ -5,7 +5,7 @@ const mustache = require('mustache');
 const converter = new showdown.Converter({ tables: true });
 
 const ctx = {
-    version: 'v0.0.1'
+    version: 'v0.0.2'
 };
 
 fs.readdirSync('./content').forEach(file => {
